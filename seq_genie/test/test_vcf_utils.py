a44d663558e30ffc0a3a8b301be8d9e87ffcb499
neilswainston <neil.swainston@manchester.ac.uk>
2019-04-02 09:52:25 +0100

    Updated test_vcf_utils method.

--- seq_genie/test/test_vcf_utils.py
+++ seq_genie/test/test_vcf_utils.py
@@ -5,38 +5,33 @@ All rights reserved.
 
 @author: neilswainston
 '''
+# pylint: disable=invalid-name
 # pylint: disable=wrong-import-order
 import os
 import unittest
 
-import pandas as pd
 from seq_genie import vcf_utils
 
 
 class Test(unittest.TestCase):
     '''Class to test utils module.'''
 
-    def test_write_summary(self):
-        '''Test demuliplex method.'''
+    def test_vcf_to_df(self):
+        '''Test vcf_to_df method.'''
         directory = os.path.dirname(os.path.realpath(__file__))
+        vcf_filename = os.path.join(directory, 'variants.vcf')
+        df, _ = vcf_utils.vcf_to_df(vcf_filename)
+        # df_filename = os.path.join(directory, 'variants_out.csv')
+        # df.to_csv(df_filename)
+        self.assertFalse(df.empty)
 
-        summary_df = pd.read_csv(os.path.join(directory, 'summary.csv'))
-        identity_df = pd.read_csv(os.path.join(directory, 'identity.csv'))
-        mutations_df = pd.read_csv(os.path.join(directory, 'mutations.csv'))
-        indels_df = pd.read_csv(os.path.join(directory, 'indels.csv'))
-        deletions_df = pd.read_csv(os.path.join(directory, 'deletions.csv'))
-        depths_df = pd.read_csv(os.path.join(directory, 'max_depths.csv'))
+    def test_analyse_vcf(self):
+        '''Test analyse_vcf method.'''
+        directory = os.path.dirname(os.path.realpath(__file__))
+        vcf_filename = os.path.join(directory, 'variants.vcf')
+        _, _, indels, _, _, _, _ = vcf_utils.analyse_vcf(vcf_filename, 0)
 
-        vcf_utils.write_summary(summary_df,
-                                identity_df,
-                                mutations_df,
-                                indels_df,
-                                deletions_df,
-                                depths_df)
-
-        summary_df.to_csv(os.path.join(directory, 'summary_out.csv'))
-
-        self.assertTrue(summary_df['identity'].between(0, 1).all())
+        self.assertAlmostEqual(indels[0][1], 0.563636, 3)
 
 
 if __name__ == "__main__":
